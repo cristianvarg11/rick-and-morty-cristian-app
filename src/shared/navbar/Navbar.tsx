0a51175ae243534c45826b4857bfb4aca4ca238f
Cristian Sanchez <cristiansanchez.itjo@gmail.com>
2023-09-10 18:29:27 -0400

    feat(Navbar): remove properties from navbar

--- src/shared/navbar/Navbar.tsx
+++ src/shared/navbar/Navbar.tsx
@@ -30,13 +30,7 @@ export default function Navbar() {
               fill="none"
               viewBox="0 0 17 14"
             >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
+              <path d="M1 1h15M1 7h15M1 13h15" />
             </svg>
           </button>
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
